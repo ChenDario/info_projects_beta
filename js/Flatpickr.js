89a0634f43fc26d3519ff8ef91e37f0184bea126
ChenDario <chen.dario@itismeucci.com>
2025-05-15 11:26:22 +0200

    add pick date cannot be greater than the to

--- js/Flatpickr.js
+++ js/Flatpickr.js
@@ -1,9 +1,25 @@
-flatpickr("#from_date", {
+let fromDatePicker, toDatePicker;
+
+fromDatePicker = flatpickr("#from_date", {
     dateFormat: "Y-m-d",
-    locale: "it"
+    locale: "it",
+    onChange: function(selectedDates) {
+        if (selectedDates.length > 0) {
+            toDatePicker.set('minDate', selectedDates[0]);
+        } else {
+            toDatePicker.set('minDate', null);
+        }
+    }
 });
 
-flatpickr("#to_date", {
+toDatePicker = flatpickr("#to_date", {
     dateFormat: "Y-m-d",
-    locale: "it"
+    locale: "it",
+    onChange: function(selectedDates) {
+        if (selectedDates.length > 0) {
+            fromDatePicker.set('maxDate', selectedDates[0]);
+        } else {
+            fromDatePicker.set('maxDate', null);
+        }
+    }
 });
